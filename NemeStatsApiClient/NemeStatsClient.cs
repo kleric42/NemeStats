9f0111e1441aabe8e60dcd0a230083598e5bbf89
Jake Gordon <jacob.gordon@Emerson.com>
2016-04-02 23:18:19 -0400

    Initial commit to set up projects

---
+++ NemeStatsApiClient/NemeStatsClient.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RestSharp;
+
+namespace NemeStatsApiClient
+{
+    public class NemeStatsClient
+    {
+        public const string BASE_URL_API_VERSION_2 = "https://nerdscorekeeper.azurewebsite.net/api/v2";
+        private IRestClient _resetClient;
+
+        public NemeStatsClient(string nemeStatsBaseApiUrl = BASE_URL_API_VERSION_2)
+        {
+            _resetClient = new RestClient(nemeStatsBaseApiUrl);
+        }
+
+
+    }
+}
---
+++ NemeStatsApiClientTests/UsersClientTests/RegisterNewUserTests.cs
@@ -0,0 +1,19 @@
+﻿using System.Configuration.Abstractions;
+using System.Linq;
+using NemeStatsApiClient;
+using NUnit.Framework;
+
+namespace NemeStatsApiClientTests.UsersClientTests
+{
+    [TestFixture]
+    public class RegisterNewUserTests
+    {
+        private NemeStatsClient _apiClient;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _apiClient = new NemeStatsClient(ConfigurationManager.Instance.AppSettings.Get("apiBaseUrl"));
+        }
+    }
+}
